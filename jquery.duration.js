06a5d44d80f1e4f3e285dfa85a1d7daf365f1f2b
Loic GUILLOIS <contact@loicguillois.fr>
2012-02-21 12:03:09 +0100

    removing IE6 support + removing french translation from main file

--- jquery.duration.js
+++ jquery.duration.js
@@ -91,19 +91,4 @@
     }
     return element.data("duration");
   }
-  
-  // fix for IE6 suckage
-  document.createElement("abbr");
-  document.createElement("time");
 }(jQuery));
-
-// French
-jQuery.duration.settings.strings = {
-   milliseconds: "%d millisecondes",
-   seconds: "%d secondes",
-   minutes: "%d minutes",
-   hours: "%d heures",
-   days: "%d jours",
-   months: "%d mois",
-   years: "%d ans"
-};
